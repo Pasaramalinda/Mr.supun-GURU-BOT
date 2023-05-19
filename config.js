caed84e3d7cbcf14c3014915fb6d88ac666a29b4
Pasaramalinda <127321008+Pasaramalinda@users.noreply.github.com>
2023-05-19 13:33:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['+1(585)2450622', 'sandaru', true],
+  ['+1(585)2450622', 'Mr.supun', true], 
   [''] 
 ] //Number of owners
 
